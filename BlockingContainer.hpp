7f7f2912b33cea428b2caa3001c463779bef30bd
Kitsune <KyuubiRan@qq.com>
2024-03-29 22:55:30 +0800

    Add try_pop_front/back for BlockingContainer

--- BlockingContainer.hpp
+++ BlockingContainer.hpp
@@ -1,65 +1,89 @@
 #pragma once
 
 #include <mutex>
+#include <optional>
 #include <condition_variable>
 
 namespace container {
+    template<typename T>
+    concept HasPopFront = requires(T a)
+    {
+        { a.pop_front() } -> std::same_as<void>;
+    };
 
-template<typename T>
-concept HasPopFront = requires(T a) {
-    { a.pop_front() } -> std::same_as<void>;
-};
+    template<typename T>
+    concept HasPopBack = requires(T a)
+    {
+        { a.pop_back() } -> std::same_as<void>;
+    };
 
-template<typename T>
-concept HasPopBack = requires(T a) {
-    { a.pop_back() } -> std::same_as<void>;
-};
+    template<typename T>
+    concept HasPushFront = requires(T a, typename T::value_type v)
+    {
+        { a.push_front(v) } -> std::same_as<void>;
+    };
 
-template<typename T>
-concept HasPushFront = requires(T a, typename T::value_type v) {
-    { a.push_front(v) } -> std::same_as<void>;
-};
+    template<typename T>
+    concept HasPushBack = requires(T a, typename T::value_type v)
+    {
+        { a.push_back(v) } -> std::same_as<void>;
+    };
 
-template<typename T>
-concept HasPushBack = requires(T a, typename T::value_type v) {
-    { a.push_back(v) } -> std::same_as<void>;
-};
+    template<typename Container>
+    class Blocking {
+    private:
+        Container m_data;
+        std::mutex m_mutex;
+        std::condition_variable m_cv;
 
-template<typename Container>
-class Blocking {
-private:
-    Container m_data;
-    std::mutex m_mutex;
-    std::condition_variable m_cv;
+    public:
+        typename Container::value_type pop_front() requires HasPopFront<Container> {
+            std::unique_lock<std::mutex> lock(m_mutex);
+            m_cv.wait(lock, [&] { return !m_data.empty(); });
+            typename Container::value_type value = m_data.front();
+            m_data.pop_front();
+            return value;
+        }
 
-public:
-    typename Container::value_type pop_front() requires HasPopFront<Container> {
-        std::unique_lock<std::mutex> lock(m_mutex);
-        m_cv.wait(lock, [&] { return !m_data.empty(); });
-        typename Container::value_type value = m_data.front();
-        m_data.pop_front();
-        return value;
-    }
+        typename Container::value_type pop_back() requires HasPopBack<Container> {
+            std::unique_lock<std::mutex> lock(m_mutex);
+            m_cv.wait(lock, [&] { return !m_data.empty(); });
+            typename Container::value_type value = m_data.back();
+            m_data.pop_back();
+            return value;
+        }
 
-    typename Container::value_type pop_back() requires HasPopBack<Container> {
-        std::unique_lock<std::mutex> lock(m_mutex);
-        m_cv.wait(lock, [&] { return !m_data.empty(); });
-        typename Container::value_type value = m_data.back();
-        m_data.pop_back();
-        return value;
-    }
+        std::optional<typename Container::value_type> try_pop_front() requires HasPopFront<Container> {
+            std::unique_lock<std::mutex> lock(m_mutex);
+            if (m_data.empty()) {
+                return std::nullopt;
+            }
+            typename Container::value_type value = m_data.front();
+            m_data.pop_front();
+            return value;
+        }
 
-    void push_back(typename Container::value_type value) requires HasPushBack<Container> {
-        std::unique_lock<std::mutex> lock(m_mutex);
-        m_data.push_back(value);
-        m_cv.notify_one();
-    }
+        std::optional<typename Container::value_type> try_pop_back() requires HasPopBack<Container> {
+            std::unique_lock<std::mutex> lock(m_mutex);
+            if (m_data.empty()) {
+                return std::nullopt;
+            }
+            typename Container::value_type value = m_data.front();
+            m_data.pop_back();
+            return value;
+        }
 
-    void push_front(typename Container::value_type value) requires HasPushFront<Container> {
-        std::unique_lock<std::mutex> lock(m_mutex);
-        m_data.push_front(value);
-        m_cv.notify_one();
-    }
-};
 
+        void push_back(typename Container::value_type value) requires HasPushBack<Container> {
+            std::unique_lock<std::mutex> lock(m_mutex);
+            m_data.push_back(value);
+            m_cv.notify_one();
+        }
+
+        void push_front(typename Container::value_type value) requires HasPushFront<Container> {
+            std::unique_lock<std::mutex> lock(m_mutex);
+            m_data.push_front(value);
+            m_cv.notify_one();
+        }
+    };
 }
--- examples/BlockingContainer.cpp
+++ examples/BlockingContainer.cpp
@@ -5,22 +5,39 @@
 #include <deque>
 
 int main() {
-    container::Blocking<std::deque<int>> container;
+    container::Blocking<std::deque<int>> container1;
+    container::Blocking<std::deque<int>> container2;
 
     auto t1 = std::thread([&] {
-        while (true) {
-            int v = container.pop_front();
+        int i = 10;
+        while (--i) {
+            int v = container1.pop_front();
             std::cout << "pop_front: " << v << std::endl;
         }
     });
 
     auto t2 = std::thread([&] {
+        int i = 10;
+        while (--i) {
+            auto v = container2.try_pop_front();
+            if (v)
+                std::cout << "try_pop_front: " << *v << std::endl;
+            else
+                std::cout << "try_pop_front: " << "empty" << std::endl;
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+    });
+
+    auto t3 = std::thread([&] {
         for (int i = 1; i <= 10; ++i) {
-            container.push_back(i);
+            container1.push_back(i);
+            container2.push_back(i);
             std::this_thread::sleep_for(std::chrono::milliseconds(200));
         }
     });
 
     t1.join();
     t2.join();
+    t3.join();
 }
