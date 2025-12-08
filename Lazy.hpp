d78cf113a2b6dec63c70abc421d87f3503aa9f5e
Kitsune <KyuubiRan@outlook.com>
2025-12-08 15:01:47 +0800

    optimize: delete Lazy copy/move ctor & add concept for get()

--- Lazy.hpp
+++ Lazy.hpp
@@ -2,15 +2,17 @@
 
 #include <optional>
 #include <functional>
+#include <mutex>
+#include <type_traits>
 
 namespace lazy {
 template<typename T, typename Ctor>
 class Lazy final {
-    std::optional<T> m_value;
-    std::optional<Ctor> m_initializer;
+    mutable std::optional<T> m_value;
+    mutable std::optional<Ctor> m_initializer;
     mutable std::once_flag m_flag;
 
-    void initialize() {
+    void initialize() const {
         std::call_once(m_flag, [this] {
             m_value = (*m_initializer)();
             m_initializer = std::nullopt;
@@ -18,20 +20,32 @@ class Lazy final {
     }
 
 public:
-    Lazy(Ctor initializer) : m_value(std::nullopt), m_initializer(std::move(initializer)) {
+    Lazy(const Lazy &) = delete;
+
+    Lazy &operator=(const Lazy &) = delete;
+
+    Lazy(Lazy &&) = delete;
+
+    Lazy &operator=(Lazy &&) = delete;
+
+    explicit Lazy(Ctor initializer) : m_value(std::nullopt), m_initializer(std::move(initializer)) {
     }
 
-    T &ref() {
+    bool initialized() const {
+        return m_value.has_value();
+    }
+
+    T &ref() const {
         initialize();
         return *m_value;
     }
 
-    T *ptr() {
+    T *ptr() const {
         initialize();
         return &*m_value;
     }
 
-    T get() {
+    T get() const requires std::copy_constructible<T> {
         initialize();
         return *m_value;
     }
